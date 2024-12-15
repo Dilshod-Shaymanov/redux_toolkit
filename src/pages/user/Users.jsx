0d5390e537b2d88be6676af9e41460f41c0a348d
Dilshod-Shaymanov <dilshodshaymanov050@gamil.com>
2024-12-15 22:41:24 +0500

    Redux toolkit

--- src/pages/user/Users.jsx
+++ src/pages/user/Users.jsx
@@ -18,7 +18,14 @@ const Users = () => {
     <div className="min-h-[70vh] bg-gradient-to-b from-[#f3f4f6] to-[#e2e8f0] py-10">
       <div className="container mx-auto px-4">
         {!users.length ? (
-         
+          <div className="flex flex-col items-center justify-center py-16">
+            <h2 className="text-5xl font-extrabold text-red-500 text-center mb-6">
+              No Users Available
+            </h2>
+            <p className="text-xl text-gray-600 text-center">
+              It looks like there are no users at the moment. Add some users to
+              see them here.
+            </p>
           </div>
         ) : (
           <UsersCart data={users} />
